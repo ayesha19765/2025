34821f10b994d9fdc9b58138927fb7c26f3f442e
ayesha19765 <123299122+ayesha19765@users.noreply.github.com>
2025-01-01 22:51:46 +0530

    Time: 19 ms (53.78%), Space: 26.2 MB (32.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -22,3 +22,28 @@ public:
         return res; // Return the final groups of anagrams
     }
 };
+/*
+Time Complexity
+Sorting Each String:
+Sorting each string takes O(LlogL), where L is the average length of strings.
+For N strings, this totals to O(N⋅LlogL).
+
+Storing in HashMap:
+Inserting each string into the hash map is O(L) for each string (string comparison is based on length).
+For N strings, this totals to O(N⋅L).
+
+Iterating Over HashMap:
+Extracting the grouped anagrams involves iterating over all keys and their values, which takes O(N⋅L).
+
+Overall, the time complexity is: O(N⋅LlogL)
+
+Space Complexity
+HashMap Storage:
+The hash map stores up to N keys (sorted strings) and their corresponding lists of strings. This takes  O(N⋅L) space in the worst case.
+
+Auxiliary Space for Sorting:
+Sorting requires 
+O(L) space for the temporary string during each sort operation.
+
+Total space complexity is: O(N⋅L)
+*/

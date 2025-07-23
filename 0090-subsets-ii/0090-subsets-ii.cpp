a5659c74c65d24a70124fee92cdf1c71fa534442
Ayesha . <ayesha19765@gmail.com>
2025-07-23 23:22:52 +0530

    Time: 0 ms (100%), Space: 10.5 MB (50.97%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, int start, vector<int>& path, vector<vector<int>>& res) {
+        res.push_back(path);
+
+        for (int i = start; i < nums.size(); ++i) {
+            // Skip duplicates at the same recursive level
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            path.push_back(nums[i]);
+            backtrack(nums, i + 1, path, res);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> path;
+        sort(nums.begin(), nums.end());  // Important to detect duplicates
+        backtrack(nums, 0, path, res);
+        return res;
+    }
+
+};

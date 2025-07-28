52f5ca7e91a7167b206f075fbb2f5c0224033db7
Ayesha . <ayesha19765@gmail.com>
2025-07-28 21:28:33 +0530

    Time: 6 ms (20.31%), Space: 17.9 MB (54.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,50 +1,20 @@
-// // Solution 1. MaxHeap, O(nlogn).
-// class Solution {
-// public:
-//     vector<int> topKFrequent(vector<int>& nums, int k) {
-//         unordered_map<int, int>m;
-//         for(auto x: nums) m[x]++;
-//         priority_queue<pair<int, int>>pq;
-//         for(auto p: m) pq.push({p.second, p.first});
-//         vector<int>res;
-//         while(k--) {
-//             res.push_back(pq.top().second);
-//             pq.pop();
-//         }
-//         return res;
-//     }
-// };
-
-
-
-// // Solution 2. MinHeap, O(nlogk).
-// class Solution {
-// public:
-//     vector<int> topKFrequent(vector<int>& nums, int k) {
-//         unordered_map<int, int>m;
-//         for(auto x: nums) m[x]++;
-//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-//         for(auto p: m){
-//             pq.push({p.second, p.first});
-//             if(pq.size() > k) pq.pop();
-//         }
-//         vector<int>res;
-//         while(k--) res.push_back(pq.top().second), pq.pop();
-//         return res;
-//     }
-// };
-
-// Solution 3. Bucket Sort, O(n).
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int>m;
-        for(auto x: nums) m[x]++;
-        vector<int>res;
-        vector<vector<int>>bucket(nums.size() + 1);
-        for(auto p: m) bucket[p.second].push_back(p.first);
-        for(int i = bucket.size() - 1; res.size() < k; i--)
-            for(auto j: bucket[i]) res.push_back(j);
+        unordered_map<int, int> freq;
+        for(int n : nums) freq[n]++;
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(auto& [num, f] : freq){
+            pq.push({f, num});
+            if(pq.size() > k) pq.pop();
+        }
+        
+        vector<int> res;
+        while(!pq.empty()){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
         return res;
     }
 };

f139355a733f1a242db3841c7e62cdc5d925e7dd
Ayesha . <ayesha19765@gmail.com>
2025-08-27 15:54:45 +0530

    Time: 5 ms (67.99%), Space: 30.3 MB (38.12%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int len = min(m, n);
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
-        int ans = 0;
+        // DP table to store size of largest square ending at (i, j)
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+        int totalSquares = 0;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                for(int start = 1; start <= min(n-j, m-i); start ++){
-                    bool flag = true;
-                    for(int cnt = 0; cnt < start; cnt++){
-                        if(!matrix[i + cnt][j + start - 1] || !matrix[i + start - 1][j + cnt])
-                        {
-                            flag = false;
-                            break;
-                        }
-                    }    
-                    ans += flag; 
-                    if(!flag)
-                        break;
-                }    
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 1) {
+                    // First row or first column → can only form 1x1 squares
+                    if (i == 0 || j == 0)
+                        dp[i][j] = 1;
+                    else
+                        // Square size depends on min of top, left, and top-left
+                        dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+
+                    // Add count of squares formed at (i, j)
+                    totalSquares += dp[i][j];
+                }
             }
         }
 
-        return ans;
+        return totalSquares;
     }
 };
-

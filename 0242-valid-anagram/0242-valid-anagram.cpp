eaea931b85dd66920ef07852b33216349e8a2973
Ayesha . <ayesha19765@gmail.com>
2026-04-11 23:57:02 +0530

    Time: 1 ms (54.52%), Space: 10.1 MB (10.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // If the strings are not of the same size, they cannot be anagrams
-        if(s.size() != t.size())
-            return false;
+        if(s.size() != t.size()) return false;
 
-        // Create a vector of 26 elements to count the occurrences of each letter
-        vector<int> cnt(26,0);
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
 
-        // Increment and decrement counts for each character in both strings respectively
-        for(int i = 0; i < s.size(); i++){
-            cnt[s[i] - 'a'] ++;
-            cnt[t[i] - 'a'] --;
-        }
-        // Check if all counts are zero, if so, strings are anagrams, otherwise, they are not
-        for(int i = 0; i < 26; i++){
-            if(cnt[i])
-                return 0;
-        }
-        return 1;
+        for(char c : s) m1[c]++;
+        for(char c : t) m2[c]++;
+
+        return m1 == m2;
     }
 };
-
-// time - O(n)
-// space - O(1)

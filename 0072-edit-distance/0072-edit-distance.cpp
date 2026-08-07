37fef1e21e8b7ad6a41a6b4f8fcdff8a95c009b9
Ayesha . <ayesha19765@gmail.com>
2026-08-07 11:47:13 +0530

    Time: 61 ms (9.69%), Space: 82.8 MB (8.69%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-        if (i < 0) return j + 1;  // Insert remaining chars of s2
-        if (j < 0) return i + 1;  // Delete remaining chars of s1
+    int solve(int i, int j, string word1, string word2, vector<vector<int>> &dp){
+        if(i == word1.size() || j == word2.size()){
+            return (word1.size() - i) + (word2.size() - j);
+        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(word1[i] == word2[j]){
+            return dp[i][j] = solve(i + 1, j + 1, word1, word2, dp);
+        }
 
-        if (dp[i][j] != -1) return dp[i][j];
+        int op1 = 1 + solve(i, j + 1, word1, word2, dp);
+        int op2 = 1 + solve(i + 1, j, word1, word2, dp);
+        int op3 = 1 + solve(i + 1, j + 1, word1, word2, dp);
 
-        if (s1[i] == s2[j])
-            return dp[i][j] = f(i - 1, j - 1, s1, s2, dp);  // No op needed
-
-        int insertOp = f(i, j - 1, s1, s2, dp);
-        int deleteOp = f(i - 1, j, s1, s2, dp);
-        int replaceOp = f(i - 1, j - 1, s1, s2, dp);
-
-        return dp[i][j] = 1 + min({insertOp, deleteOp, replaceOp});
+        return dp[i][j] = min({op1, op2, op3});
     }
-
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, word1, word2, dp);
+        vector<vector<int>> dp(word1.size(), vector<int> (word2.size(), -1));
+        return solve(0, 0, word1, word2, dp);
     }
-
 };

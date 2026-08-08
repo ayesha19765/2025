88b424966541d5fa9da79eb7d9af07a6840d8970
Ayesha . <ayesha19765@gmail.com>
2026-08-08 22:08:52 +0530

    Time: 0 ms (100%), Space: 11.8 MB (91.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,45 +1,35 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
-    unordered_map<Node*, Node*> visited;
 
     Node* cloneGraph(Node* node) {
-        if (!node) return nullptr;
+        if (node == nullptr)
+            return nullptr;
 
-        // If node is already cloned, return it
-        if (visited.find(node) != visited.end()) {
-            return visited[node];
+        unordered_map<Node*, Node*> mp;
+        
+        queue<Node*> q;
+        mp[node] = new Node(node->val);
+        q.push(node);
+
+        while (!q.empty())
+        {
+            Node* curr = q.front();
+            q.pop();
+
+            for (Node* neighbor : curr->neighbors)
+            {
+                // Clone neighbor if not visited
+                if (!mp.count(neighbor))
+                {
+                    mp[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+
+                // Connect cloned nodes
+                mp[curr]->neighbors.push_back(mp[neighbor]);
+            }
         }
 
-        // Clone the current node
-        Node* clone = new Node(node->val);
-        visited[node] = clone;
-
-        // Clone all neighbors
-        for (Node* neighbor : node->neighbors) {
-            clone->neighbors.push_back(cloneGraph(neighbor));
-        }
-
-        return clone;
+        return mp[node];
     }
 };
